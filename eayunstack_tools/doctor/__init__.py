c20c332df6c4eecae8fdf8b8cf9d1fd3d1508a76
Dunrong Huang <dunrong.huang@eayun.com>
2015-03-02 16:21:16 +0800

    doctor: review
    Signed-off-by: Dunrong Huang <dunrong.huang@eayun.com>

--- eayunstack_tools/doctor/__init__.py
+++ eayunstack_tools/doctor/__init__.py
@@ -1,22 +1,6 @@
-import pkg_resources
-import sys
+from eayunstack_tools import utils
+
 
 def make(parser):
     '''EayunStack Doctor'''
-    subp = parser.add_subparsers(
-        title='Commands',
-        metavar='COMMAND',
-        help='description',
-        )
-
-    entry_points = [
-        (e.name, e.load()) for e in pkg_resources.iter_entry_points('doctor_command')
-    ]
-    for (name, fn) in entry_points:
-        p = subp.add_parser(
-            name,
-            description=fn.__doc__,
-            help=fn.__doc__,
-        )
-        fn(p)
-    return parser
+    return utils.make_subcommand(parser, 'doctor')
