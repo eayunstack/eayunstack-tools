83e368abbf972dba6a0a98c6ea1187b5152e958d
Coffee <coffee.zyr@gmail.com>
2015-04-09 19:43:00 +0800

    rename function list_backup() -> list_bck to avoid conflict, write_db() didn't rewrite

--- eayunstack_tools/fuel/backup.py
+++ eayunstack_tools/fuel/backup.py
@@ -1,5 +1,6 @@
-afrom utils import backup_list
-from utils import read_db, write_db
+# @file backup.py
+from eayunstack_tools.fuel.utils import list_backup
+from eayunstack_tools.fuel.utils import read_db, write_db
 import commands
 import logging
 
@@ -7,36 +8,38 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+
 def backup(parser):
     if parser.NEW_BACKUP:
         new_backup()
     elif parser.LIST_BACKUP:
-        list_backup()
+        list_bck()
     else:
-        list_backup()
+        list_bck()
 
 def make(parser):
     '''Fuel Backup'''
     parser.add_argument(
         '-n',
         '--new',
-        action = 'store_true',
-        dest = 'NEW_BACKUP',
-        default = False,
-        help = 'Start A New Backup'
-    )   
+        action='store_true',
+        dest='NEW_BACKUP',
+        default=False,
+        help='Start A New Backup'
+    )
     parser.add_argument(
         '-l',
         '--list',
-        action = 'store_true',
-        dest = 'LIST_BACKUP',
-        default = False,
-        help = 'List All Backups'
-    )   
+        action='store_true',
+        dest='LIST_BACKUP',
+        default=False,
+        help='List All Backups'
+    )
     parser.set_defaults(func=backup)
 
+
 def new_backup():
-    LOG.info('Starting Backup ...')
+    LOG.info('Starting backup ...')
     LOG.info('It will take about 30 minutes, Please wait ...')
     (stat, out) = commands.getstatusoutput('dockerctl backup')
     if stat != 0:
@@ -46,18 +49,18 @@ def new_backup():
         print 'You can use "eayunstack fuel backup [ -l | --list ]" to list your backups\n'
         # 1) read db to get last_id
         lines = read_db()
-        id = 1
+        backup_id = 1
         if len(lines) == 0:
-            write_db(id)
+            write_db(backup_id)
         else:
             # 2) create new id
-            id = int(lines[-1].split(' ')[0])
-            id += 1
+            backup_id = int(lines[-1].split(' ')[0])
+            backup_id += 1
             # 3) write to db
-            write_db(id)
+            write_db(backup_id)
 
-def list_backup():
-    t = backup_list()
-    print t.get_string(sortby = 'ID')
+def list_bck():
+    t = list_backup()
+    print t.get_string(sortby='ID')
 
 
