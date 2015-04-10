c425aaef2770b6994773b1656cd292d874778795
Coffee <coffee.zyr@gmail.com>
2015-04-10 16:41:32 +0800

    add check_db(): check db (sync or not) before list

--- eayunstack_tools/fuel/utils.py
+++ eayunstack_tools/fuel/utils.py
@@ -10,6 +10,17 @@ BACKUP_DIR = '/var/backup/fuel'
 
 def list_backup():
     """List all the backup file"""
+    # check db before list backup
+    check_list = check_db()
+    if check_list == 1:
+        pass
+    else:
+        try:
+            with open('/tmp/tools.db', 'w') as db:
+                for backup_id in check_list.keys():
+                    write_db(backup_id, check_list[backup_id])
+        except Exception as e:
+            LOG.error('Write to db error!')
     lines = read_db()
     i = 1
     t = PrettyTable(['ID', 'Backup Time', 'Backup File'])
@@ -30,7 +41,7 @@ def list_backup():
         c_time = file_split[3].split('.', 1)[0][:2] \
                  + ':' \
                  + file_split[3].split('.', 1)[0][2:]
-        t.add_row([i, c_date + ' ' + c_time, backup_file])
+        t.add_row([backup_id, c_date + ' ' + c_time, backup_file])
         i += 1
     return t
 
@@ -67,14 +78,55 @@ def latest_backup():
                 latest_backup = os.listdir(BACKUP_DIR + '/' \
                                 + backup_dirs[-i] + '/')[0]
                 return latest_backup
-                break
 
-
-def write_db(backup_id):
+def write_db(backup_id, backup_file):
     # append
-    db = open('/tmp/tools.db', 'a')
-    backup_file = latest_backup()
-    db.writelines('%s' % backup_id + ' ' + '%s\n' % backup_file)
-    db.close()
+    try:
+        with open('/tmp/tools.db', 'a') as db:
+            db.writelines('%s' % backup_id + ' ' + '%s\n' % backup_file)
+    except Exception as e:
+        LOG.error('Write to db error!')
 
-
+def check_db():
+    """Check if db sync with /var/backup/fuel"""
+    backup_dirs = os.listdir(BACKUP_DIR)
+    backup_files = []
+    backup_dict = {}
+    # Output the backup file under /var/backup/fuel as a list
+    for backup_dir in backup_dirs:
+        if os.path.isfile(BACKUP_DIR + '/' + backup_dir):
+            backup_files.append(backup_dir)
+        else:
+            # if delete the backup file but didn't delete backup dir, won't write to backup_files
+            if os.listdir(BACKUP_DIR + '/' + backup_dir) == []:
+                pass
+            else:
+                backup_files.append(os.listdir(BACKUP_DIR + '/' + backup_dir)[0])
+    list_old = read_db()
+    backup_list = read_db()
+    print backup_files
+    i = 0
+    # Compare backup_list and backup_files
+    try:
+        while True:
+            # if tools.db was empty
+            if len(backup_list) == 0:
+                break
+            # if backup_list sync with backup_files, do nothing
+            elif backup_list[i].split(' ')[1].strip('\n') in backup_files:
+                i += 1
+            # if not, means backup_file has been deleted, deleted from backup_list
+            else:
+                backup_list.remove(backup_list[i])
+    except Exception:
+        pass
+    finally:
+        if list_old == backup_list:
+            return 1 # true
+        else:
+            # Output the new backup_list as a dict
+            for line in backup_list:
+                line = line.strip('\n')
+                # backup_dict = {'id': 'backup_file'}
+                backup_dict[line.split(' ')[0]] = line.split(' ')[1]
+            return backup_dict
