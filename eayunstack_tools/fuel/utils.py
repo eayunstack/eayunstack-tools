762940974cc139bdbb75d05910bdf1503bcbf3d3
Dunrong Huang <dunrong.huang@eayun.com>
2015-04-15 10:27:01 +0800

    fuel: list_backup 只有在 backup 模块用到， 不用 utils
    Signed-off-by: Dunrong Huang <dunrong.huang@eayun.com>

--- eayunstack_tools/fuel/utils.py
+++ eayunstack_tools/fuel/utils.py
@@ -1,49 +1,10 @@
 # @file utils.py
 import os
-from prettytable import PrettyTable
 import logging
 
 LOG = logging.getLogger(__name__)
 
 BACKUP_DIR = '/var/backup/fuel'
-
-
-def list_backup():
-    """List all the backup file"""
-    # check db before list backup
-    check_list = check_db()
-    if check_list == 1:
-        pass
-    else:
-        try:
-            # TODO: db is not used?
-            with open('/tmp/tools.db', 'w') as db:
-                for backup_id in check_list.keys():
-                    write_db(backup_id, check_list[backup_id])
-        except Exception:
-            LOG.error('Write to db error!')
-    lines = read_db()
-    i = 1
-    t = PrettyTable(['ID', 'Backup Time', 'Backup File'])
-    for line in lines:
-        # Delete the '\n' at the end of the line
-        # line = 'id backup_file_name\n'
-        # e.g. '1 fuel_backup_2015-04-09_0831.tar.lrz'
-        line = line.strip('\n')
-        backup_id = line.split(' ')[0]
-        # backup_file = fuel_backup_2015-04-09_0831.tar.lrz
-        backup_file = line.split(' ')[1]
-        # file_split = ['fuel', 'backup', '2015-04-09', '0831.tar.lrz']
-        file_split = backup_file.split('_', 4)
-        # Get the backup time from filename
-        # c_date = '2015=04-09'
-        c_date = file_split[2]
-        # c_time = '08:31'
-        c_time = file_split[3].split('.', 1)[0][:2] + ':' + \
-            file_split[3].split('.', 1)[0][2:]
-        t.add_row([backup_id, c_date + ' ' + c_time, backup_file])
-        i += 1
-    return t
 
 
 def read_db():
