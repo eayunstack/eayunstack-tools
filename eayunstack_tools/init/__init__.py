44b4f8267c3410829481ba7a2e4bcec5e2599b94
blkart <blkart.org@gmail.com>
2015-04-22 11:25:28 +0800

    eayunstack init sub-cmd: add init_node_list_file func
    Signed-off-by: blkart <blkart.org@gmail.com>

---
+++ eayunstack_tools/init/__init__.py
@@ -0,0 +1,48 @@
+import sys
+import logging
+import os
+from fuelclient.client import APIClient
+from eayunstack_tools.utils import scp_connect
+
+LOG = logging.getLogger(__name__)
+
+def make(parser):
+    '''EayunStack Environment Initialization'''
+    if len(sys.argv) < 3:
+        init_node_list_file()
+
+def init_node_list_file():
+    # generate node-list file
+    LOG.info('Generate node-list file ...')
+    file_path = '/.eayunstack/node-list'
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.mkdir(os.path.dirname(file_path))
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    logging.disable(logging.CRITICAL)
+    rep = APIClient.get_request("nodes/")
+    logging.disable(logging.NOTSET)
+    ips = []
+    for node in rep:
+        fqdn = node['fqdn']
+        ip = node['ip']
+        ips.append(ip)
+        roles = ''
+        if len(node['roles']) > 1:
+            for n in node['roles']: 
+                if not roles:
+                    roles = roles + n
+                else:
+                    roles = roles + ',' + n
+        else:
+            roles = node['roles'][0]
+        host = fqdn.split('.')[0]
+        entry = fqdn + ':' + host + ':' + ip + ':' + roles + '\n'
+        output = open(file_path,'a')
+        output.write(entry)
+        output.close()
+    # scp node-list file to all nodes
+    LOG.info('Copy node-list file to all nodes ...')
+    for ip in ips:
+        LOG.info('   To node %s ...' % ip)
+        scp_connect(ip, file_path, file_path)
