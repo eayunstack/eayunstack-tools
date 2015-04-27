ce96d25323bac17668da43f3d46867a7a7b574c3
blkart <blkart.org@gmail.com>
2015-04-27 18:05:38 +0800

    manage: modify argument
    Signed-off-by: blkart <blkart.org@gmail.com>

--- eayunstack_tools/manage/volume.py
+++ eayunstack_tools/manage/volume.py
@@ -5,6 +5,13 @@ LOG = logging.getLogger(__name__)
 
 def volume(parser):
     print "reference module"
+    if parser.DESTROY_VOLUME:
+        if not parser.ID:
+            LOG.error('Please use [--id ID] to specify the volume ID !')
+        else:
+            volume_id = parser.ID
+            global volume_id
+            destroy_volume()
 
 def make(parser):
     '''Volume Management'''
@@ -18,18 +25,21 @@ def make(parser):
     parser.add_argument(
         '-d',
         '--destroy-volume',
-        action='store_const',
-        const='destroy_volume',
-        help='Destroy Error Volumes'
+        dest='DESTROY_VOLUME',
+        action='store_true',
+        default=False,
+        help='Destroy Volume'
     )
     parser.add_argument(
         '--id',
-        help='Volumes ID'
+        action='store',
+        dest='ID',
+        help='Volume ID'
     )
     parser.set_defaults(func=volume)
 
 def list_errors():
     print "List Error Volume"
 
-def destroy_volume(volume_id):
+def destroy_volume():
     print "Destroy Error Volume: %s" % volume_id
