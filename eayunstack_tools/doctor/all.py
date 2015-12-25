4fcce9136d548d3178a1aff3c5e35a1529f72ffe
blkart <blkart.org@gmail.com>
2015-12-25 11:35:49 +0800

    [doctor][all] change the output format
    Signed-off-by: blkart <blkart.org@gmail.com>

--- eayunstack_tools/doctor/all.py
+++ eayunstack_tools/doctor/all.py
@@ -5,11 +5,11 @@ from eayunstack_tools.doctor import env, cls, stack
 from eayunstack_tools.logger import StackLOG as LOG
 
 def check_all(parser):
-    LOG.info('%s %-27s %s' % ('+'*25, 'Check Basic Environment', '+'*25))
+    LOG.info('%s %-27s %s' % ('+'*13, 'Check Basic Environment', '+'*13))
     env.check_all()
-    LOG.info('%s %-27s %s' % ('+'*25, 'Check Cluster Environment', '+'*25))
+    LOG.info('%s %-27s %s' % ('+'*13, 'Check Cluster Environment', '+'*13))
     cls.check_all()
-    LOG.info('%s %-27s %s' % ('+'*25, 'Check OpenStack Environment', '+'*25))
+    LOG.info('%s %-27s %s' % ('+'*13, 'Check OpenStack Environment', '+'*13))
     stack.check_all()
 
 def make(parser):
