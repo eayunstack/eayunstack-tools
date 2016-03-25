f924bb7c7c57670da207cc03ae2705396c9e87c6
CoffeeChou <coffee.zyr@gmail.com>
2016-03-25 14:51:56 +0800

    use os.environ to get credentials
    Signed-off-by: CoffeeChou <coffee.zyr@gmail.com>

--- eayunstack_tools/credentials.py
+++ eayunstack_tools/credentials.py
@@ -1,48 +1,40 @@
+import os
 import commands
 
 ENV_VAR_FILE = '/root/openrc'
 
-(s, o) = commands.getstatusoutput('source %s; \
-                                echo OS_AUTH_URL=$OS_AUTH_URL; \
-                                echo OS_USERNAME=$OS_USERNAME; \
-                                echo OS_PASSWORD=$OS_PASSWORD; \
-                                echo OS_TENANT_NAME=$OS_TENANT_NAME; \
-                                echo OS_REGION_NAME=$OS_REGION_NAME' \
-                                % ENV_VAR_FILE)
-if s == 0:
-    var = {}
-    for v in o.split('\n'):
-        var[v.split('=')[0]]=v.split('=')[1]
+(s, o) = commands.getstatusoutput('source %s' % ENV_VAR_FILE)
+
 
 def get_nova_credentials_v2():
     d = {}
     d['version'] = '2'
-    d['username'] = var['OS_USERNAME']
-    d['api_key'] = var['OS_PASSWORD']
-    d['auth_url'] = var['OS_AUTH_URL']
-    d['project_id'] = var['OS_TENANT_NAME']
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
     return d
 
 def get_cinder_credentials():
     d = {}
-    d['username'] = var['OS_USERNAME']
-    d['api_key'] = var['OS_PASSWORD']
-    d['auth_url'] = var['OS_AUTH_URL']
-    d['project_id'] = var['OS_TENANT_NAME']
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
     return d
 
 def get_neutron_credentials():
     d = {}
-    d['username'] = var['OS_USERNAME']
-    d['password'] = var['OS_PASSWORD']
-    d['auth_url'] = var['OS_AUTH_URL']
-    d['tenant_name'] = var['OS_TENANT_NAME']
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['tenant_name'] = os.environ['OS_TENANT_NAME']
     return d
 
 def get_keystone_credentials():
     d = {}
-    d['username'] = var['OS_USERNAME']
-    d['password'] = var['OS_PASSWORD']
-    d['auth_url'] = var['OS_AUTH_URL']
-    d['tenant_name'] = var['OS_TENANT_NAME']
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['tenant_name'] = os.environ['OS_TENANT_NAME']
     return d
